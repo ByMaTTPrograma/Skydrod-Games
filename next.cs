cfd0eeaa1b8bc7ffcae0f3938861f651b3c859dd
ByMaTTPrograma <40031228+ByMaTTPrograma@users.noreply.github.com>
2018-06-07 20:13:18 +0200

    Add files via upload

---
+++ next.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class next : MonoBehaviour {
+	
+	public GameObject Mario;
+	public GameObject Bowser;
+	
+	void OnMouseDown(){
+		
+		if(Input.GetMouseButtonDown(0)){
+			if(Mario.activeSelf){
+				
+				Mario.SetActive (false);
+				Bowser.SetActive (true);
+				
+			}else if(Bowser.activeSelf){
+				
+				Mario.SetActive (true);
+				Bowser.SetActive (false);
+				
+			}
+			
+			
+		}
+		
+	}
+	
+}
